b0fd2e0e1de171415610af9bf9bdc9b0c169da9e
Navpan18 <npanchayan.official@gmail.com>
2021-12-05 15:01:11 +0530

    Updates

---
+++ precache-manifest.d752ed8ec0e188b8b27cc0a457bba913.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a7e0943fda238fde90bf0ffdeb64f0",
+    "url": "/covid-19-tracker-final/index.html"
+  },
+  {
+    "revision": "6e81383def2ced98f663",
+    "url": "/covid-19-tracker-final/static/css/2.7d011da2.chunk.css"
+  },
+  {
+    "revision": "67df4e6d6fd8b1726f1f",
+    "url": "/covid-19-tracker-final/static/css/main.f84c4e49.chunk.css"
+  },
+  {
+    "revision": "6e81383def2ced98f663",
+    "url": "/covid-19-tracker-final/static/js/2.4211423c.chunk.js"
+  },
+  {
+    "revision": "07649aa805f82c2be4955036047ddb0a",
+    "url": "/covid-19-tracker-final/static/js/2.4211423c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e86b5585959759efb6",
+    "url": "/covid-19-tracker-final/static/js/3.0ab33c37.chunk.js"
+  },
+  {
+    "revision": "67df4e6d6fd8b1726f1f",
+    "url": "/covid-19-tracker-final/static/js/main.b2944654.chunk.js"
+  },
+  {
+    "revision": "ad825b4793cdcc268e52",
+    "url": "/covid-19-tracker-final/static/js/runtime-main.38e6eabc.js"
+  }
+]);
